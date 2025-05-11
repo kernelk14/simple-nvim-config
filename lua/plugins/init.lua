09e1d6e5a3f8a34bb794e819775daddb5b4cdc6d
kernelk14 <khyleisaias@gmail.com>
2025-05-11 19:09:34 +0800

    Added trouble.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -84,6 +84,43 @@ require("lazy").setup({
         { 'ms-jpq/coq.artifacts', branch = 'artifacts' },
         { 'ms-jpq/coq.thirdparty', branch = '3p' },
     },
+    {
+      "folke/trouble.nvim",
+      opts = {},
+      cmd = "Trouble",
+      keys = {
+        {
+          ";xx",
+          "<cmd>Trouble diagnostics toggle<cr>",
+          desc = "Diagnostics (Trouble)",
+        },
+       {
+          ";xX",
+          "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+          desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+          ";cs",
+          "<cmd>Trouble symbols toggle focus=false<cr>",
+          desc = "Symbols (Trouble)",
+        },
+        {
+          ";cl",
+          "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+          desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+          ";xL",
+          "<cmd>Trouble loclist toggle<cr>",
+          desc = "Location List (Trouble)",
+        },
+        {
+          ";xQ",
+          "<cmd>Trouble qflist toggle<cr>",
+          desc = "Quickfix List (Trouble)",
+        },
+      },
+    },
 
   },
   install = { colorscheme = { "tokyonight-night" } },
