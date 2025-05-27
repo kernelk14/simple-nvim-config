ec402b0d11e8d8cbc9a7617aeffb1a09a439d25b
kernelk14 <khyleisaias@gmail.com>
2025-05-27 08:39:34 +0800

    Updated blink.cmp

--- lua/settings/lspconfig.lua
+++ lua/settings/lspconfig.lua
@@ -83,3 +83,18 @@ lsp.omnisharp.setup({
     root_dir = util.root_pattern('*.sln', '*.csproj', 'omnisharp.json', 'function.json'),
 })
 
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities({}, false))
+capabilities = vim.tbl_deep_extend('force', capabilities, {
+  textDocument = {
+    foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true
+    }
+  }
+})
+
+for _, server in ipairs(lsps) do
+    lsp[server].setup{ capabilities = capabilities }
+end
--- lua/settings/lsps.lua
+++ lua/settings/lsps.lua
@@ -3,29 +3,24 @@ require("mason-lspconfig").setup({
     automatic_enable = false
 })
 
-require('blink.cmp').setup ({
-    keymap = {
-    -- set to 'none' to disable the 'default' preset
-    preset = 'default',
-  
-    ['<S-Tab>'] = { 'select_prev', 'fallback' },
-    ['<Tab>'] = { 'select_next', 'fallback' },
-  
-    -- disable a keymap from the preset
-    ['<C-e>'] = {},
-    
-    -- show with a list of providers
-    ['<C-space>'] = { function(cmp) cmp.show({ providers = { 'snippets' } }) end },
-  
-  }
-})
-
 local lspconfig = require("lspconfig")
 
 local servers = {'clangd', 'html', 'cssls','ols', 'gopls'}
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities({}, false))
+
+capabilities = vim.tbl_deep_extend('force', capabilities, {
+  textDocument = {
+    foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true
+    }
+  }
+})
+
 for _, lsp in ipairs(servers) do
-    capabilities = require('blink.cmp').get_lsp_capabilities()
     lspconfig[lsp].setup{ capabilities = capabilities }
 end
 
